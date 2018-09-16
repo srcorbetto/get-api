2be41e1a7029d70b48c5cb85560e55c04ddf67ac
Mike Corbett <srcorbetto@gmail.com>
2018-09-16 16:34:34 -0500

    Testing Weather API

--- server.js
+++ server.js
@@ -5,25 +5,35 @@ const port = process.env.PORT || 5000;
 
 require('dotenv').config();
 
+// Allow for CORS
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
-app.get('/api/hello', (req, res) => {
-    console.log(req.params);
-  res.send({ express: 'Hello From Express' });
-});
-
 // GIPHY Weather API - .gif ID
 app.get('/api/gif/:id', (req, res) => {
   axios.get('https://api.giphy.com/v1/gifs/' + req.params.id + '?api_key=' + process.env.GIPHY_WEATHER_API_KEY)
   .then(response => {
-    console.log(response.data.data.images.fixed_height.url);
       res.send({
         data: response.data.data.images.fixed_height.url
       });
+  })
+  .catch(function (error) {
+      console.log(error);
+      return error
+    });
+});
+
+// GIPHY Weather API - Get weather
+app.get('/api/weather/:city', (req, res) => {
+  axios.get('https://api.openweathermap.org/data/2.5/weather?q=' + req.params.city + '&appid=' + process.env.OPEN_WEATHER_GIPHY_WEATHER_API_KEY)
+  .then(response => {
+      res.send({
+        data: response.data
+      });
+      console.log(response.data);
   })
   .catch(function (error) {
       console.log(error);
